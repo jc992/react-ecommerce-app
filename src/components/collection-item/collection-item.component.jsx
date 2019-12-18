74ee6feb0e4a2d54e727df45222bee1250eec9bf
JoaoT <joaot@agap2.pt>
2019-12-18 17:02:06 -0100

    finished refactoring remaining components to use styled components instead of .scss styling

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,30 +1,22 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-
-import CustomButton from '../custom-button/custom-button.component';
 import { addItem } from '../../redux/cart/cart.actions';
 
-import './collection-item.styles.scss';
+import { CollectionItemContainer, BackgroundImage, CollectionFooterContainer, AddButton, NameContainer, PriceContainer } from './collection-item.styles';
 
 
 const CollectionItem = ({ item, addItem }) => {
     const { imageUrl, name, price } = item;
     return (
-        <div className='collection-item'>
-            <div
-                className='image'
-                style={{
-                    backgroundImage: `url(${imageUrl})`
-                }}
-            />
-
-            <div className='collection-footer'>
-                <span className='name'>{name}</span>
-                <span className='price'>€{price}</span>
-            </div>
-            <CustomButton onClick={() => addItem(item)} inverted> Add to cart </CustomButton>
-        </div>
+        <CollectionItemContainer>
+            <BackgroundImage className='image' imageUrl={imageUrl}/>
+            <CollectionFooterContainer>
+                <NameContainer>{name}</NameContainer>
+                <PriceContainer>€{price}</PriceContainer>
+            </CollectionFooterContainer>
+            <AddButton className='button' onClick={() => addItem(item)} inverted> Add to cart </AddButton>
+        </CollectionItemContainer>
     )
 };
 
